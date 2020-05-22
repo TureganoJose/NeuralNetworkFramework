3a1a17a579c7b4a98f614d401904a41080fe5e3f
Jose Martinez <turegano.jose@gmail.com>
2020-05-22 14:59:35 +0100

    Rearranged the Neural network and layer classes so they come together (CNNs and DNNs). I guess this is just another iteration in the process.

--- ConvNeuralNetworkFramework.py
+++ ConvNeuralNetworkFramework.py
@@ -3,10 +3,10 @@ import scipy as sp
 import gzip
 import pickle
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from sklearn.datasets import make_moons
-from PIL import Image
-from tensorflow.keras import datasets
+# from matplotlib.animation import FuncAnimation
+# from sklearn.datasets import make_moons
+# from PIL import Image
+# from tensorflow.keras import datasets
 
 import math
 
@@ -100,7 +100,7 @@ def column_to_image(cols, images_shape, filter_shape, stride, output_shape='same
     # Return image without padding
     return images_padded[:, :, pad_h[0]:height+pad_h[0], pad_w[0]:width+pad_w[0]]
 
-class Conv2d():
+class Conv2d:
     """A 2D Convolution Layer.
     Parameters:
     -----------
@@ -120,6 +120,7 @@ class Conv2d():
     """
     def __init__(self, n_filters, filter_shape, input_shape=None, padding='same', stride=1, activation_function='ReLU'):
         self.n_filters = n_filters
+        self.input_data = []
         self.filter_shape = filter_shape
         self.padding = padding
         self.stride = stride
@@ -173,7 +174,7 @@ class Conv2d():
 
     def forward_pass(self, X, training=True):
         batch_size, channels, height, width = X.shape
-        self.layer_input = X
+        self.input_data = X
         # Turn image shape into column shape
         # (enables dot product between input and weights)
         self.X_col = image_to_column(X, self.filter_shape, stride=self.stride, output_shape=self.padding)
@@ -199,20 +200,16 @@ class Conv2d():
             # The gradient with respect to bias terms is the sum similarly to in Dense layer
             d_b = np.sum(delta, axis=1, keepdims=True)
 
-            # Update the layers weights
-            self.W = self.W_opt.update(self.W, d_w)
-            self.b = self.w0_opt.update(self.b, d_b)
-
         # Recalculate the gradient which will be propagated back to prev. layer
         delta = self.W_col.T.dot(delta)
         # Reshape from column shape to image shape
         da_dz = self.grad_activation(self, self.output)
         delta = column_to_image(delta,
-                                self.layer_input.shape,
+                                self.input_data.shape,
                                 self.filter_shape,
                                 stride=self.stride,
                                 output_shape=self.padding) * da_dz
-        return delta
+        return delta, d_w, d_b
 
     def output_shape(self):
         channels, height, width = self.input_shape
@@ -222,7 +219,7 @@ class Conv2d():
         return self.n_filters, int(output_height), int(output_width)
 
 
-class Flatten():
+class Flatten:
     """ Turns a multidimensional matrix into two-dimensional """
     def __init__(self, input_shape=None):
         self.prev_shape = None
@@ -244,12 +241,11 @@ class Flatten():
 class Dense:
     """Basic definition of layer for neural networks"""
 
-    def __init__(self, raw_data: np.ndarray, n_nodes: int, activation_function: str):
+    def __init__(self, input_shape: list, n_nodes: int, activation_function: str):
         # Create Attributes
-        self.raw_data = raw_data
         self.input_data = []
         self.n_nodes = n_nodes
-        self.n_dim, self.n_samples = raw_data.shape  # columns indicate the number of training samples
+        self.n_dim, self.n_samples = input_shape  # columns indicate the number of training samples
         self.activation_function = activation_function
         self.trainable = True
 
@@ -263,6 +259,7 @@ class Dense:
         self.delta = []
 
     def forward_propagation(self, input_data):
+        self.input_data = input_data
         self.z = np.dot(self.W, input_data) + self.b
         self.a = self.forward_activation(self.z)
         return self.a
@@ -272,20 +269,17 @@ class Dense:
         W = self.W
 
         if self.trainable:
-            # Calculate gradient w.r.t layer weights
-            d_w = self.layer_input.T.dot(delta)
-            d_b = np.sum(delta, axis=0, keepdims=True)
-
-            # Update the layer weights
-            self.W = self.W_opt.update(self.W, d_w)
-            self.b = self.w0_opt.update(self.b, d_b)
+            # Calculate gradient w.r.t layer weights (includes activation layer hence da/dz)
+            da_dz = self.grad_activation(self.z)
+            delta = delta * da_dz
+            d_w = np.dot(delta, self.input_data.T)
+            d_b = np.sum(delta, axis=1, keepdims=True)
 
         # Return accumulated gradient for next layer
         # Calculated based on the weights used during the forward pass
-        da_dz = self.grad_activation(self, self.z)
-        delta = delta.dot(W.T) * da_dz
+        delta = np.dot(W.T,delta)
         # np.multiply(dJ_da, da_dz)
-        return delta
+        return delta, d_w, d_b
 
     def forward_activation(self, z):
         if self.activation_function == "sigmoid":
@@ -329,71 +323,209 @@ class Dense:
         return np.array([self.n_nodes, self.n_samples])
 
 
-class Dense(Layer):
-    """A fully-connected NN layer.
-    Parameters:
-    -----------
-    n_units: int
-        The number of neurons in the layer.
-    input_shape: tuple
-        The expected input shape of the layer. For dense layers a single digit specifying
-        the number of features of the input. Must be specified if it is the first layer in
-        the network.
-    """
-    def __init__(self, n_units, input_shape=None):
-        self.layer_input = None
-        self.input_shape = input_shape
-        self.n_units = n_units
-        self.trainable = True
-        self.W = None
-        self.w0 = None
+class Network:
+    """ Class contains layers"""
 
-    def initialize(self, optimizer):
-        # Initialize the weights
-        limit = 1 / math.sqrt(self.input_shape[0])
-        self.W  = np.random.uniform(-limit, limit, (self.input_shape[0], self.n_units))
-        self.w0 = np.zeros((1, self.n_units))
+    def __init__(self, layers: list, raw_data: np.ndarray, output_targets: np.array, lambd: float, learning_rate: float, n_epoch: int, n_batch_size: int, loss_function: str):
+        self.layers = layers  # This is a list of layer classes
+        self.raw_data = raw_data
+        self.n_dim, self.n_samples = raw_data.shape  # columns indicate the number of training samples
+        self.lambd = lambd # Regularization
+        self.loss_function = loss_function
+        self.output_values = []
+        self.matrix_cost = []
+        self.output_targets = output_targets
+        self.learning_rate = learning_rate
+        self.dJ_da = []
+        self.n_epoch = n_epoch
+        self.n_batch_size = n_batch_size
+        self.batched_input = []
+        self.batched_output = []
+        self.n_batches = []
+
+    def forward_pass(self, input_data):
+        output_values = []
+        for ilayer, layer in enumerate(self.layers):
+            if ilayer == 0:
+                output_values = self.layers[ilayer].forward_propagation(input_data)
+            else:
+                output_values = self.layers[ilayer].forward_propagation(output_values)
+        self.output_values = output_values
+        return self.output_values
+
+    def dJ_dw_diff(self, ilayer_w, j, k, offset):
+        temp_outputs = self.forward_pass_offset(ilayer_w, j, k, offset)
+        delta_J = self.calculate_cost_pred(temp_outputs) - self.calculate_cost_pred(self.output_values)
+        delta_w = offset
+        return delta_J/delta_w
+
+    def dJ_db_diff(self, ilayer_b, j, b_offset):
+        temp_outputs = self.forward_pass_boffset(ilayer_b, j, b_offset)
+        delta_J = self.calculate_cost_pred(temp_outputs) - self.calculate_cost_pred(self.output_values)
+        delta_b = b_offset
+        return delta_J/delta_b
+
+    def calculate_matrix_cost(self):
+        # self.output_values - self.output_targets
+        # (((1-self.output_targets)*np.log(1-self.output_values)+self.output_targets*np.log(self.output_values)))
+        self.matrix_cost = - np.multiply(self.output_targets, np.log(self.output_values))
+
+    def calculate_total_cost(self):
+        # - np.sum(((1-self.output_targets)*np.log(1-self.output_values)+self.output_targets*np.log(self.output_values)))/self.n_samples
+        return - (1/self.n_samples)*np.sum(np.multiply(self.output_targets, np.log(self.output_values)))
+
+    def calculate_cost_pred(self, input_predictions, input_targets):
+        self.lambd_cost = 0
+        if self.lambd > 0.0:
+            for ilayer, layer in enumerate(self.layers):
+                self.lambd_cost = np.sum(self.layers[ilayer].W**2)
+        self.lambd_cost *= self.lambd / (2 * self.n_samples)
+        # - np.sum(((1-self.output_targets)*np.log(1-self.output_values)+self.output_targets*np.log(self.output_values)))/self.n_samples
+        if self.loss_function == "cross_entropy":  # Binary classification
+            return - (1/self.n_samples) * np.sum(np.nan_to_num(-input_targets*np.log(input_predictions)-(1-input_targets)*np.log(1-input_predictions))) + self.lambd_cost
+        elif self.loss_function == "mse":
+            return np.sqrt( (1.0 / self.n_samples) * np.sum((input_predictions - input_targets)**2)) + self.lambd_cost
+        elif self.loss_function == "log_likelihood":  # Multi-class
+            return - (1/self.n_samples) * np.sum(np.log(input_predictions)) + self.lambd_cost
+
+    def calculate_loss_gradient(self, predictions, output_targets):
+        delta = output_targets - predictions
+        return delta
+
+    def derivative_cost(self, predictions, targets):
+        #  (self.matrix_cost - self.output_targets)/(self.matrix_cost - np.multiply(self.matrix_cost, self.matrix_cost))
+        """Derivative of cost relative to a or last layer's activation function, softmax usually dJ_da"""
+        #https://condor.depaul.edu/~ntomuro/courses/578/notes/3-Backprop-More.pdf
+        #return np.divide(-self.output_targets[:, isample][np.newaxis].transpose(), predictions)
+        if self.loss_function == "cross_entropy":  # Binary classification
+            return -np.divide(predictions - targets, predictions * (1-predictions))
+        elif self.loss_function == "mse":
+            return -(targets - predictions)
+        elif self.loss_function == "log_likelihood":  # Multi-class
+            return -np.divide(predictions - targets, predictions * (1-predictions))
 
 
-    def parameters(self):
-        return np.prod(self.W.shape) + np.prod(self.w0.shape)
+    # an auxiliary function that converts probability into class
+    def convert_prob_into_binary(self, predictions, threshold):
+        predictions_binary = np.copy(predictions)
+        predictions_binary[predictions_binary > threshold] = 1
+        predictions_binary[predictions_binary <= threshold] = 0
+        return predictions_binary
 
-    def forward_pass(self, X, training=True):
-        self.layer_input = X
-        return X.dot(self.W) + self.w0
+    def get_accuracy(self, predictions):
+        prediction_binary = self.convert_prob_into_binary(predictions, 0.5)
+        return (prediction_binary == self.output_targets).all(axis=0).mean()
 
-    def backward_pass(self, accum_grad):
-        # Save weights used during forwards pass
-        W = self.W
+    def backpropagation(self, input_data, output_targets):
+        n_samples_per_batch = input_data.shape[1]
+        predictions = self.forward_pass(input_data)
+        # First calculate error (self.delta) of output layer
+        delta = self.calculate_loss_gradient( predictions, output_targets)
+        L = len(self.layers)
+        for iLayer in range(L-1, -1, -1):
+            delta, d_w, d_b = self.layers[iLayer].backward_pass(delta)
+            # Update weights
+            self.layers[iLayer].W += (self.learning_rate) * d_w / n_samples_per_batch
+            self.layers[iLayer].b += (self.learning_rate) * d_b / n_samples_per_batch
 
-        if self.trainable:
-            # Calculate gradient w.r.t layer weights
-            grad_w = self.layer_input.T.dot(accum_grad)
-            grad_w0 = np.sum(accum_grad, axis=0, keepdims=True)
+    def create_minibatches(self):
+        if self.n_samples % self.n_batch_size != 0:
+            print("Warning: create_minibatches(): Batch size {0} does not evenly divide the number of examples {1}.".format(self.n_batch_size, self.n_samples))
+        batched_input = []
+        batched_output = []
+        idx = 0
+        while idx + self.n_batch_size <= self.n_samples:
+            batched_input.append(self.raw_data[:, idx:idx+self.n_batch_size])
+            batched_output.append(self.output_targets[:, idx:idx+self.n_batch_size])
+            idx += self.n_batch_size
 
-            # Update the layer weights
-            self.W = self.W_opt.update(self.W, grad_w)
-            self.w0 = self.w0_opt.update(self.w0, grad_w0)
+        return batched_input, batched_output
 
-        # Return accumulated gradient for next layer
-        # Calculated based on the weights used during the forward pass
-        accum_grad = accum_grad.dot(W.T)
-        return accum_grad
+    def plot_dec_bound(self, X, iepoch):
+        # Set min and max values and give it some padding
+        x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+        y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+        h = 0.01
+        # Generate a grid of points with distance h between them
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        # Predict the function value for the whole grid
+        Z = self.forward_pass(np.c_[xx.ravel(), yy.ravel()].T)
+        Z = Z.reshape(xx.shape)
+        # Plot the contour and training examples
+        plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+        plt.scatter(X[0, :], X[1, :], c=colour, cmap=plt.cm.Spectral)
+        plt.ylabel('x2')
+        plt.xlabel('x1')
+        plt.title('Decision Boundary - L2 Regularization 0.05 Epoch {0}'.format(iepoch))
+        plt.savefig('testplot'+str(iepoch)+'.png')
+        #plt.show()
+        return
+
+    def training(self):
+        #if self.n_batch_size > 1:
+        self.batched_input, self.batched_output = self.create_minibatches()
+        self.n_batches = len(self.batched_input)
+        # images = []
+        # icount=0
+        for iepoch in range(self.n_epoch):
+            for ibatch in range(self.n_batches):
+                self.backpropagation(self.batched_input[ibatch], self.batched_output[ibatch])
+            predictions = self.forward_pass(self.raw_data)
+            accuracy = self.get_accuracy(predictions)
+            losses = self.calculate_cost_pred(predictions, self.output_targets)
+            print("epoch", iepoch,"loss", losses, "accuracy", accuracy)
+        #     icount += 1
+        #     if icount > 5:
+        #         icount = 0
+        #         self.plot_dec_bound(self.raw_data, iepoch)
+        #         ims = Image.open('testplot' + str(iepoch) + '.png')
+        #         images.append(ims)
+        # images[0].save('regu_0_05.gif',
+        #                save_all=True, append_images=images[1:], optimize=False, duration=400, loop=0)
 
 
 
 
+#Load the MNIST data
+def load_data_shared(filename="C:/Workspaces/NeuralNetworkFramework/data/mnist.pkl.gz"):
+    f = gzip.open(filename, 'rb')
+    training_data, validation_data, test_data = pickle.load(f, encoding="latin1")
+    f.close()
+    return training_data, validation_data, test_data
 
-# https://www.tensorflow.org/tutorials/images/cnn
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
-train_images = np.asarray(train_images)
-train_labels = np.asarray(train_labels)
-test_images = np.asarray(test_images)
-test_labels = np.asarray(test_labels)
+training_data, validation_data, test_data = load_data_shared()
+# Only 5000 samples otherwise running out of memory
+training_data_input = training_data[0][0:50000].transpose()
+# One-hot encoding the outputs
+training_data_outputs = np.zeros((10, training_data_input.shape[1]))
+for iPos in range(training_data_input.shape[1]):
+    training_data_outputs[training_data[1][iPos]][iPos] = 1
 
-# Normalize pixel values to be between 0 and 1
-train_images, test_images = train_images / 255.0, test_images / 255.0
+
+layer1 = Dense(training_data_input.shape, 200, "sigmoid")
+layer2 = Dense(layer1.get_output_size(), 80, "sigmoid")
+layer3 = Dense(layer2.get_output_size(), 10, "sigmoid")
+
+layer_list = [layer1, layer2, layer3]
+
+NN = Network(layer_list, training_data_input, training_data_outputs, lambd=0.02, learning_rate=0.5, n_epoch=10, n_batch_size=10, loss_function="cross_entropy")
+
+NN.training()
+
+
+
+
+# # https://www.tensorflow.org/tutorials/images/cnn
+# (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+#
+# train_images = np.asarray(train_images)
+# train_labels = np.asarray(train_labels)
+# test_images = np.asarray(test_images)
+# test_labels = np.asarray(test_labels)
+#
+# # Normalize pixel values to be between 0 and 1
+# train_images, test_images = train_images / 255.0, test_images / 255.0
 
 
 # Tensorflow layer
